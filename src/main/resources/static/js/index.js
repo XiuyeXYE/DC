c5bcfd709485cc4a2f40492f4ae38551def53080
xiuye <xiuye_engineer@outlook.com>
2020-03-13 16:09:38 +0800

    nothing

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,11 +4,13 @@
 
 // xy.DEBUG_TIME = false;
 
+
+
 runtime(xy.ready,xy,
 /* xy("ready", */()=>{
 	xdebug("websocket demo");
 	
-	let ws = new WebSocket("ws://localhost:8080/im/user");
+	let ws = new WebSocket("ws://"+document.location.host+"/im/user");
 	xdebug(ws);
 	let i = 0;
 	xy.copy(ws,{
@@ -37,26 +39,30 @@ runtime(xy.ready,xy,
 	(stream)=>{
 		
 		xdebug("open stream");
-		let ctx = new AudioContext();
-		ctx.createMediaStreamSource(stream).connect(ctx.destination);
+// let ctx = new AudioContext();
+// ctx.createMediaStreamSource(stream).connect(ctx.destination);
 		xdebug("connected");
-		xdebug("dest:",ctx.destination);
+// xdebug("dest:",ctx.destination);
 		xdebug(stream);
 		xdebug("audio tracks=",stream.getAudioTracks());
 		xdebug("video tracks=",stream.getVideoTracks());
 		xdebug("tracks=",stream.getTracks());
-		let video = xy.byId('mp4').get();
-		video.srcObject = stream;
-		video.onloadedmetadata = function(e) {
-		    video.play();
-		};
+// let video = xy.byId('mp4').get();
+// video.srcObject = stream;
+// video.onloadedmetadata = function(e) {
+// video.play();
+// };
+		
+		xy.byId('mp4').kv('srcObject',stream).kv('onloadedmetadata',(e)=>{
+			e.target.play();
+		});
 	}
 // ,
 // (err)=>{
 // xdebug(err);
 // }
 	).catch((err)=>{
-		xdebug(err);
+		xdebug(err,err.name,err.message);
 	});
 	
 });
