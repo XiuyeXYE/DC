56813b266b272543249103fef3808186ded71191
xiuye <xiuye_engineer@outlook.com>
2020-03-25 15:50:16 +0800

    nothing

--- src/test/java/com/xy/dc/DcApplicationTests.java
+++ src/test/java/com/xy/dc/DcApplicationTests.java
@@ -1,5 +1,7 @@
 package com.xy.dc;
 
+import com.xiuye.compiler.util.XYCompiler;
+import com.xiuye.util.cls.TypeUtil;
 import com.xiuye.util.log.LogUtil;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,7 @@ class DcApplicationTests {
 	@Test
 	void contextLoads() {
 		LogUtil.log(123);
+		XYCompiler.compileCode("",TypeUtil.createMap());
 	}
 
 }
